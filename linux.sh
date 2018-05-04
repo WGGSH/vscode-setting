4807a368574f3bfd73e060b6b242c264ed3a53f2
wgg <is0332ks@ed.ritsumei.ac.jp>
2018-05-04 19:06:47 +0900

    Linux用修正

--- linux.sh
+++ linux.sh
@@ -5,6 +5,12 @@
 # keybindings.json, settings.json, snippets etc...
 ln -s "`pwd`/User" ${HOME}/.config/Code/
 
+# Markdown Preview Enhancedのcssファイルへのリンクを追記
+echo "@import url(\"${HOME}/markdown_pdf.css\");" >> ~/.mume/style.less
+
+# cssファイルのリンクを作成
+ln -s "`pwd`/markdown_pdf.css" ${HOME}/markdown_pdf.css
+
 # 拡張機能のインストール
 filename="./extension-list"
 cat ${filename} | while read line
